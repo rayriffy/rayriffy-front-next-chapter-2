528a7085e3baffec3afe4f518d4125431a892948
Phumrapee Limpianchop <git@rayriffy.com>
2020-12-02 03:57:25 +0900

    fix: image url

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -23,7 +23,7 @@ fetch('https://blog.rayriffy.com/api/author/rayriffy?page=1')
     <article class="xl:w-1/4 md:w-1/2 p-4">
       <a href="${blog.url}" rel="noopener noreferrer" target="_blank">
         <div class="bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="${blog.banner}" alt="Banner">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="${blog.banner.url}" alt="Banner">
           <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font uppercase">${blog.categoryCollection.items[0].name}</h3>
           <h2 class="text-lg text-gray-900 dark:text-white font-medium title-font mb-2">${blog.title}</h2>
           <p class="leading-relaxed text-base">${blog.subtitle}</p>
