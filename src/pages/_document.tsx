72ba57f02e77d6ff9fc5fa6eaef5169841cd5f57
Phumrapee Limpianchop <git@rayriffy.com>
2021-06-16 11:27:11 +0000

    chore: font optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,9 +29,10 @@ class NextDocument extends Document {
             content="Welcome to the front frontier of rayriffy.com!"
           />
 
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800&display=swap"
             rel="stylesheet"
-            href="https://rsms.me/inter/inter.css"
             media="screen,print"
           />
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -37,7 +37,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
